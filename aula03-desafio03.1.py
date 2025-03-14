e1fe69f53ff48b30f37d6ebb57af7ed31606777f
felipefvalle <felipefranck0@gmail.com>
2025-03-13 23:45:11 -0300

    Create aula03-desafio03.1.py

---
+++ aula03-desafio03.1.py
@@ -0,0 +1,22 @@
+print("Calculadora Básica.")
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+
+if num2 != 0:
+    divisao = num1 / num2
+else:
+    divisao = "Indefinida (não é possível dividir por zero)"
+
+print("\nResultados:")
+
+print(f"Soma: {soma}")
+
+print(f"Subtração: {subtracao}")
+
+print(f"Multiplicação: {multiplicacao}")
+
+print(f"Divisão: {divisao}")
