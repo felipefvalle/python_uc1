371dd4283c388ca65fc0419e384c2335bc859984
felipefvalle <felipefranck0@gmail.com>
2025-03-19 01:42:20 -0300

    Create aula06_desafio06.16.py

---
+++ aula06_desafio06.16.py
@@ -0,0 +1,9 @@
+numero = 1
+while numero <= 5:
+    print(f"Tabuada do {numero}:")
+    multiplicador = 1
+    while multiplicador <= 10:
+        print(f"{numero} x {multiplicador} = {numero * multiplicador}")
+        multiplicador += 1
+    print()  
+    numero += 1
